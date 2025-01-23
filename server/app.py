664baedf857b206cfb5de6cd9dab27fad3aa9907
Maxi-arch <mxmu025@gmail.com>
2025-01-23 11:05:25 +0300

    1stcommit

--- server/app.py
+++ server/app.py
@@ -23,12 +23,30 @@ def clear_session():
 @app.route('/articles')
 def index_articles():
 
-    pass
+    articles = Article.query.all()
+    articles_data = [{
+        'id' : article.id,
+        'title' : article.title
+    } for article in articles]
+    return jsonify(articles_data),200
 
 @app.route('/articles/<int:id>')
 def show_article(id):
 
-    pass
+    session['page_views'] = session.get('page_views',0)
+    session['page_views'] += 1
+    if session['page_views'] <= 3:
+        article = Article.query.get(id)
+        if article:
+            return jsonify({
+                'id':article.id,
+                'title':article.title,
+                'content':article.content
+            }),200
+        else:
+            return{'message': '404:Article not found.'},404
+    else:
+        return {'message':'Maximum pageview limit reached'},401
 
 if __name__ == '__main__':
     app.run(port=5555)
